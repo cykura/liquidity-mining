21579cd7ffdf36301070f7822644c155c5caab70
zkwarlock <shardul.aeer@somaiya.edu>
2022-02-15 16:05:45 +0530

    feat: Locker and governor setup
    governor must be a smart wallet owner

--- tests/liquidity-mining.ts
+++ tests/liquidity-mining.ts
@@ -1,12 +1,13 @@
+import * as chai from "chai";
 import * as anchor from '@project-serum/anchor';
 import { Program, web3 } from '@project-serum/anchor';
 import { PublicKey, SolanaProvider } from '@saberhq/solana-contrib';
 import { GokiSDK, SmartWalletWrapper } from '@gokiprotocol/client';
 import { chaiSolana, expectTX } from '@saberhq/chai-solana';
-import * as chai from "chai";
-import { LiquidityMining } from '../target/types/liquidity_mining';
 import { ASSOCIATED_TOKEN_PROGRAM_ID, createMint, TOKEN_PROGRAM_ID } from '@saberhq/token-utils';
 import { Token } from '@solana/spl-token';
+import { findGovernorAddress, findLockerAddress, GovernorWrapper, LockerWrapper, TribecaSDK } from '@tribecahq/tribeca-sdk';
+import { LiquidityMining } from '../target/types/liquidity_mining';
 
 chai.use(chaiSolana);
 
@@ -22,17 +23,35 @@ describe('liquidity-mining', () => {
 
   const program = anchor.workspace.LiquidityMining as Program<LiquidityMining>;
 
+  // base address to derive smart wallet and governor addresses
+  const base = web3.Keypair.generate();
+
   // goki smart wallet
   let gokiSdk: GokiSDK
-  const smartWalletBase = web3.Keypair.generate();
   const numOwners = 10;
   const ownerA = web3.Keypair.generate();
-  const owners = [ownerA.publicKey];
+  let owners: PublicKey[];
+  // const owners = [ownerA.publicKey];
   const threshold = new anchor.BN(1);
   let smartWalletWrapper: SmartWalletWrapper;
 
   // governance token
   let govTokenMint: PublicKey;
+
+  // Tribeca governor
+  let tribecaSdk: TribecaSDK
+  let lockerKey: PublicKey
+  let governorKey: PublicKey
+  let governorWrapper: GovernorWrapper
+  let lockerWrapper: LockerWrapper
+
+  it('derive addresses', async () => {
+    [governorKey] = await findGovernorAddress(base.publicKey);
+    [lockerKey] = await findLockerAddress(base.publicKey);
+
+    owners = [governorKey]
+  })
+
   it('setup goki multisig smart wallet', async () => {
     gokiSdk = GokiSDK.load({ provider: solanaProvider });
 
@@ -41,7 +60,7 @@ describe('liquidity-mining', () => {
         numOwners,
         owners,
         threshold,
-        base: smartWalletBase,
+        base: base,
       }
     );
     await expectTX(tx, "create new smartWallet").to.be.fulfilled;
@@ -83,6 +102,27 @@ describe('liquidity-mining', () => {
     await anchorProvider.send(mintTokenTx)
   })
 
+  it('Governor and locker setup', async () => {
+    tribecaSdk = TribecaSDK.load({ provider: solanaProvider });
+
+    // create governor
+    const { wrapper: governorWrapperInner, tx: createGovernorTx } = await tribecaSdk.govern.createGovernor({
+      electorate: lockerKey,
+      smartWallet: smartWalletWrapper.key,
+      baseKP: base,
+    })
+    await expectTX(createGovernorTx).to.be.fulfilled;
+    governorWrapper = governorWrapperInner;
+
+    // create locker
+    const { tx: createLockerTx } = await tribecaSdk.createLocker({
+      baseKP: base,
+      governor: governorKey,
+      govTokenMint,
+    })
+    await expectTX(createLockerTx).to.be.fulfilled;
+
+  })
 
   it('Is initialized!', async () => {
     // Add your test here.

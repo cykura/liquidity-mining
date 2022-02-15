31e89a6895ba8752cba625bd68c21131735cc6a0
zkwarlock <shardul.aeer@somaiya.edu>
2022-02-15 14:38:27 +0530

    feat: Init goki SDK

--- tests/liquidity-mining.ts
+++ tests/liquidity-mining.ts
@@ -1,13 +1,25 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
+import { SolanaProvider } from '@saberhq/solana-contrib';
+import { GokiSDK } from '@gokiprotocol/client';
 import { LiquidityMining } from '../target/types/liquidity_mining';
 
 describe('liquidity-mining', () => {
+  const anchorProvider = anchor.Provider.env();
+  anchor.setProvider(anchorProvider);
 
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  const solanaProvider = SolanaProvider.init({
+    connection: anchorProvider.connection,
+    wallet: anchorProvider.wallet,
+    opts: anchorProvider.opts,
+  });
 
   const program = anchor.workspace.LiquidityMining as Program<LiquidityMining>;
+
+  let gokiSdk: GokiSDK
+  it('setup goki multisig smart wallet', async () => {
+    gokiSdk = GokiSDK.load({ provider: solanaProvider })
+  })
 
   it('Is initialized!', async () => {
     // Add your test here.

5916123251e76114e4948dc5aeed0103d658c1be
zkwarlock <shardul.aeer@somaiya.edu>
2022-02-15 15:00:27 +0530

    feat: Smart wallet multisig setup

--- tests/liquidity-mining.ts
+++ tests/liquidity-mining.ts
@@ -1,8 +1,12 @@
 import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
+import { Program, web3 } from '@project-serum/anchor';
 import { SolanaProvider } from '@saberhq/solana-contrib';
-import { GokiSDK } from '@gokiprotocol/client';
+import { GokiSDK, SmartWalletWrapper } from '@gokiprotocol/client';
+import { chaiSolana, expectTX } from '@saberhq/chai-solana';
+import * as chai from "chai";
 import { LiquidityMining } from '../target/types/liquidity_mining';
+
+chai.use(chaiSolana);
 
 describe('liquidity-mining', () => {
   const anchorProvider = anchor.Provider.env();
@@ -17,8 +21,29 @@ describe('liquidity-mining', () => {
   const program = anchor.workspace.LiquidityMining as Program<LiquidityMining>;
 
   let gokiSdk: GokiSDK
+  const smartWalletBase = web3.Keypair.generate();
+  const numOwners = 10; // Big enough.
+
+  const ownerA = web3.Keypair.generate();
+  const owners = [ownerA.publicKey];
+
+  const threshold = new anchor.BN(1);
+
+  let smartWalletWrapper: SmartWalletWrapper;
+
   it('setup goki multisig smart wallet', async () => {
-    gokiSdk = GokiSDK.load({ provider: solanaProvider })
+    gokiSdk = GokiSDK.load({ provider: solanaProvider });
+
+    const { smartWalletWrapper: wrapperInner, tx } = await gokiSdk.newSmartWallet(
+      {
+        numOwners,
+        owners,
+        threshold,
+        base: smartWalletBase,
+      }
+    );
+    await expectTX(tx, "create new smartWallet").to.be.fulfilled;
+    smartWalletWrapper = wrapperInner;
   })
 
   it('Is initialized!', async () => {

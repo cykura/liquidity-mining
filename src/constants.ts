8d7515c480040706fdb32f17b9cfaa4207fff2d0
zkwarlock <shardul.aeer@somaiya.edu>
2022-04-28 16:43:41 +0530

    feat: Cykura DAO constants

--- src/constants.ts
+++ src/constants.ts
@@ -35,3 +35,18 @@ export const CYKURA_CODERS = buildCoderMap<{
 const MAX_INCENTIVE_DURATION = 63072000;
 /// The max amount of seconds into the future the incentive start_time can be set
 const MAX_INCENTIVE_START_LEAD_TIME = 2592000;
+
+// DAO parameters for Cykura
+// https://tribeca.so/gov/HnV7iBwe3pfeUdmBwmNir8jDWUunsb7CuGSHnnBdMJBC
+export const TRIBECA_PARAMS = {
+    governor: new web3.PublicKey("HnV7iBwe3pfeUdmBwmNir8jDWUunsb7CuGSHnnBdMJBC"),
+    locker: new web3.PublicKey("6vGZ4L4QpDDEU8uet6TGJCJZSLcWg2yjRKFkNFumdZUn"),
+    token: new web3.PublicKey("BRLsMczKuaR5w9vSubF4j8HwEGGprVAyyVgS4EX7DKEg"),
+    quorum: 2000000000000,
+    minVotes: 100000000000,
+    timelockDelay: 1,
+    votingDelay: 1,
+    minStakeDuration: 86400, // 1 day
+    maxStakeDuration: 157680000, // 5 years
+    maxStakeVoteMultiplier: 10,
+}
